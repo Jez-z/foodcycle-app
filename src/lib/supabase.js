95a1d7a5a446a96f53508d69c77594ac7b7656c1
Irene Felicia <irenefelicia556@gmail.com>
2026-05-19 20:45:57 +0700

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,1 +1,9 @@
+import { createClient } from "@supabase/supabase-js";
 
+const supabaseUrl = "https://nrgkhibnlosutauenrvp.supabase.co";
+const supabaseKey = "sb_publishable_C33j6PTWWevn0Bdk5-LLXQ_muNUzdKK";
+
+export const supabase = createClient(
+  supabaseUrl,
+  supabaseKey
+);
