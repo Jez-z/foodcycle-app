f8f13719d7f37572a3104a9eadf81e886dae3995
billeyyy <hbllfrz1218@gmail.com>
2026-05-20 04:10:20 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,6 @@ import { supabase } from "./supabaseClient";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import OrderList from "./pages/OrderList";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
 
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
@@ -90,16 +88,6 @@ function App() {
         <Route
           path="/orders"
           element={<OrderList />}
-        />
-
-        <Route
-          path="/login"
-          element={<Login />}
-        />
-
-        <Route
-          path="/register"
-          element={<Register />}
         />
 
         <Route
